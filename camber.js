6305a31c62451e7fab982a8423e16ab39c0c82aa
James <erresen@users.noreply.github.com>
2017-04-28 21:06:50 +0100

    Started styling

---
+++ camber.js
@@ -0,0 +1,38 @@
+$(function(){
+    $('.update-results').on('change blur keyup', updateResults);
+    updateResults();
+});
+
+function updateResults () {
+    var top = parseFloat($('#top').val());
+    var bottom = parseFloat($('#bottom').val());
+    var diameter = parseFloat($('#wheel-diameter').val());
+    var camber = calculateCamber(top, bottom, diameter);
+    displayResults(camber);
+}
+
+function displayResults (camber) {
+    var orientation = '';
+    var angleText = '?';
+    if(!isNaN(camber)){
+        if (camber > 0) {
+            orientation = 'positive ';
+        } else if(camber < 0){
+            orientation = 'negative ';                
+        }
+        var absCamber = Math.abs(camber);
+        angleText = absCamber.toFixed(2) + '&deg;';
+    }
+    $('.angle').html(angleText);
+    $('.orientation').html(orientation + 'camber');
+}
+
+function calculateCamber (top, bottom, diameter) {
+    var offset = bottom - top;
+    var camber = Math.sin(offset / diameter);
+    return radToDegrees(camber);
+}
+
+function radToDegrees (angle) {
+    return angle * (180 / Math.PI);
+}
